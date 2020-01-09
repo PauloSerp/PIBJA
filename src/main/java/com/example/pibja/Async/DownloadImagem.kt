74dafcb1efa0e5b26c99258f845c19480550ae3f
paulo <pauloserp@github.com>
2020-01-09 12:26:32 -0300

    Lista de programação completa

--- src/main/java/com/example/pibja/Async/DownloadImagem.kt
+++ src/main/java/com/example/pibja/Async/DownloadImagem.kt
@@ -3,24 +3,41 @@ package com.example.pibja.Async
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
+import com.example.pibja.R
+import java.lang.Exception
 import java.net.URL
 
 class DownloadImagem(val imageView: ImageView, val progress: ProgressBar): AsyncTask<String, Void, Bitmap>(){
-    override fun doInBackground(vararg params: String?): Bitmap {
+    override fun doInBackground(vararg params: String?): Bitmap? {
         val url = params[0]
 
-        val stream = URL(url).openStream()
-        val bitmap = BitmapFactory.decodeStream(stream)
+        try {
+            val stream = URL(url).openStream()
+            val bitmap = BitmapFactory.decodeStream(stream)
 
-        return bitmap
+            return bitmap
+        }catch (e: Exception){
+            Log.e("DownloadImagem", ""+e)
+            return null
+        }
+
     }
 
     override fun onPostExecute(result: Bitmap?) {
-        progress.visibility = View.GONE
-        imageView.setImageBitmap(result)
+        if (result != null){
+            progress.visibility = View.GONE
+            imageView.setImageBitmap(result)
+        }else{
+            progress.visibility = View.GONE
+            imageView.setImageResource(R.drawable.errobaixar)
+            Log.e("DownloadImagem", "Erro ao baixar")
+        }
+
     }
 
 
